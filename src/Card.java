62cadc96d9047733476910df862656a7351d8a64
Student <Student@avd8pc005.psn.pleasanton.k12.ca.us>
2022-02-11 10:34:44 -0800

    william

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,19 @@
-
+import java.awt.Graphics;
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
+    private int value;
+    private int suit;
+    private boolean color;
+    private boolean faceup;
 
+    public Card(int value, int suit){
+        this.value=value;
+        this.suit=suit;
+    }
+    public int getSuit(){
+        return suit;
+    }
+    public int getValue(){
+        return value;
+    }
 }
